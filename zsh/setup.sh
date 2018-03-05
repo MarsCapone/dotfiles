0475ec0a26ea52da5bcfe204425ab6b6c6999346
Samson Danziger <samson.danziger@citrix.com>
2018-03-05 17:30:34 +0000

    Add zsh install script

--- zsh/setup.sh
+++ zsh/setup.sh
@@ -1,5 +1,16 @@
+#!/bin/sh
+
 # Setup script for zsh
 
+# remove old stuff
 rm -rf ~/.zshrc   > /dev/null 2>&1
 rm -rf ~/.zsh     > /dev/null 2>&1
+rm -rf ~/.zsh-dircolors.config > /dev/null 2>&1
 
+# prepare
+mkdir -p ~/.zsh
+curl -sL git.io/antibody | bash -s
+
+# make links
+ln -sf $(readlink -f zshrc) ~/.zshrc
+antibody bundle < plugins.txt > ~/.zsh/plugins.sh
