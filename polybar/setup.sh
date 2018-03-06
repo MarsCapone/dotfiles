4e50896c989855fb9f037179b66fd18ff0d53c1e
Samson Danziger <samson@samsondanziger.com>
2018-03-06 09:51:19 +0000

    Add networkmanager_dmenu to polybar config

--- polybar/setup.sh
+++ polybar/setup.sh
@@ -4,11 +4,20 @@ rm -rf ~/.config/polybar/config > /dev/null 2>&1
 rm -rf ~/.config/polybar/master.conf > /dev/null 2>&1
 rm -rf ~/.config/polybar/custom.conf > /dev/null 2>&1
 
+rm -rf ~/.config/networkmanager_demu/config.ini > /dev/null 2>&1
+
+sudo rm $(which networkmanager_dmenu)
+
 mkdir -p ~/.config/polybar
+mkdir -p ~/.config/networkmanager_dmenu
+
 
 ln -sf $(readlink -f config) ~/.config/polybar/config
 ln -sf $(readlink -f master.conf) ~/.config/polybar/master.conf
 ln -sf $(readlink -f custom.conf) ~/.config/polybar/custom.conf
+
+ln -sf $(readlink -f networkmanager_dmenu-config.ini) ~/.config/networkmanager_dmenu/config.ini
+sudo ln -sf $(readlink -f networkmanager_dmenu) /usr/sbin/networkmanager_dmenu
 
 polybar -r top &
 polybar -r bottom &
