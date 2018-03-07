5800023d05f6f2d08efb47feda9fefc847c0761a
Samson Danziger <samson.danziger@citrix.com>
2018-03-07 13:43:46 +0000

    Test out zgen config

--- zsh/setup.sh
+++ zsh/setup.sh
@@ -6,11 +6,10 @@
 rm -rf ~/.zshrc   > /dev/null 2>&1
 rm -rf ~/.zsh     > /dev/null 2>&1
 rm -rf ~/.zsh-dircolors.config > /dev/null 2>&1
+rm -rf ~/.config/zgen > /dev/null 2>&1
 
 # prepare
-mkdir -p ~/.zsh
-curl -sL git.io/antibody | bash -s
+git clone https://github.com/tarjoilija/zgen.git "${HOME}/.config/zgen"
 
 # make links
 ln -sf $(readlink -f zshrc) ~/.zshrc
-antibody bundle < plugins.txt > ~/.zsh/plugins.sh
